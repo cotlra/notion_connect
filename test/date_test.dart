326c141267c15e5468349c85fc786f702eb22a81
cotlra <ia15illust@gmail.com>
2025-03-29 15:53:13 +0900

    delete test folder

--- test/date_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:notion_connect/src/model/common/date.dart';
-
-void main() {
-  test('', () {
-    final date = Date(2025, 10, 11);
-    final date2 = Date(2025, 10, 12);
-  });
-}
