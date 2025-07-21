517e922e753674e76fed69e2083c53c6454685c4
cotlra <ia15illust@gmail.com>
2025-07-21 12:06:24 +0900

    Freezedクラスの形式を整理

--- lib/src/model/page_or_database/notion_page_or_database/notion_page_or_database.dart
+++ lib/src/model/page_or_database/notion_page_or_database/notion_page_or_database.dart
@@ -11,47 +11,6 @@ import '../notion_database_property/notion_database_property.dart';
 part 'notion_page_or_database.freezed.dart';
 part 'notion_page_or_database.g.dart';
 
-@Freezed(unionKey: 'object')
-sealed class NotionPageOrDatabase with _$NotionPageOrDatabase {
-  const factory NotionPageOrDatabase.page({
-    String? id,
-    DateTime? createdTime,
-    NotionUser? createdBy,
-    DateTime? lastEditedTime,
-    NotionUser? lastEditedBy,
-    bool? archived,
-    bool? inTrash,
-    NotionIcon? icon,
-    NotionFile? cover,
-    Map<String, NotionPageProperty>? properties,
-    NotionParent? parent,
-    String? url,
-    String? publicUrl,
-  }) = NotionPage;
-  const factory NotionPageOrDatabase.database({
-    String? id,
-    DateTime? createdTime,
-    NotionUser? createdBy,
-    DateTime? lastEditedTime,
-    NotionUser? lastEditedBy,
-    List<NotionRichText>? title,
-    List<NotionRichText>? description,
-    NotionIcon? icon,
-    NotionFile? cover,
-    Map<String, NotionDatabaseProperty>? properties,
-    NotionParent? parent,
-    String? url,
-    bool? archived,
-    bool? inTrash,
-    bool? isInline,
-    String? publicUrl,
-  }) = NotionDatabase;
-  const NotionPageOrDatabase._();
-
-  factory NotionPageOrDatabase.fromJson(Map<String, dynamic> json) =>
-      _$NotionPageOrDatabaseFromJson(json);
-}
-
 @freezed
 abstract class NotionPage extends NotionPageOrDatabase with _$NotionPage {
   const factory NotionPage({
@@ -100,4 +59,45 @@ abstract class NotionDatabase extends NotionPageOrDatabase
 
   factory NotionDatabase.fromJson(Map<String, dynamic> json) =>
       _$NotionDatabaseFromJson(json);
+}
+
+@Freezed(unionKey: 'object')
+sealed class NotionPageOrDatabase with _$NotionPageOrDatabase {
+  const factory NotionPageOrDatabase.page({
+    String? id,
+    DateTime? createdTime,
+    NotionUser? createdBy,
+    DateTime? lastEditedTime,
+    NotionUser? lastEditedBy,
+    bool? archived,
+    bool? inTrash,
+    NotionIcon? icon,
+    NotionFile? cover,
+    Map<String, NotionPageProperty>? properties,
+    NotionParent? parent,
+    String? url,
+    String? publicUrl,
+  }) = NotionPage;
+  const factory NotionPageOrDatabase.database({
+    String? id,
+    DateTime? createdTime,
+    NotionUser? createdBy,
+    DateTime? lastEditedTime,
+    NotionUser? lastEditedBy,
+    List<NotionRichText>? title,
+    List<NotionRichText>? description,
+    NotionIcon? icon,
+    NotionFile? cover,
+    Map<String, NotionDatabaseProperty>? properties,
+    NotionParent? parent,
+    String? url,
+    bool? archived,
+    bool? inTrash,
+    bool? isInline,
+    String? publicUrl,
+  }) = NotionDatabase;
+  const NotionPageOrDatabase._();
+
+  factory NotionPageOrDatabase.fromJson(Map<String, dynamic> json) =>
+      _$NotionPageOrDatabaseFromJson(json);
 }
